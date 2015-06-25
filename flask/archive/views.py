4cce063f0a62a0078e2ee5da85bc81e7943d4238
Ubuntu <ubuntu@ip-172-31-2-89.us-west-1.compute.internal>
2015-06-25 04:28:36 +0000

    new front end and kafka scripts

---
+++ flask/archive/views.py
@@ -0,0 +1,16 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
---
+++ kafka/run_camus.sh
@@ -0,0 +1,2 @@
+cd /usr/local/hadoop/etc/hadoop/
+hadoop jar camus-example-0.1.0-SNAPSHOT-shaded.jar com.linkedin.camus.etl.kafka.CamusJob -P ~/gitgraph/camus/camus-example/src/main/resources/camus.properties
