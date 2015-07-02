e87575e96d1e3cbe19e0b3ea5d8b027c155aad50
Ubuntu <ubuntu@ip-172-31-2-89.us-west-1.compute.internal>
2015-07-02 00:49:33 +0000

    kafka cleaned

--- kafka/check_topic.sh
+++ kafka/check_topic.sh
@@ -1,3 +1,8 @@
+# This script checks Kafka topic details. The topic name is passed as the command line argument.
+# e.g.- ./check_topic.sh topic-1
+# The above statement would give details like which node is the leader for the topic, the number of partitions 
+# and the replication factor.
+
 #!/usr/bin/env bash
 
 /usr/local/kafka/bin/kafka-topics.sh --describe --zookeeper localhost:2181 --topic $1
--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -1,3 +1,8 @@
+# This script creates a new Kafka topic. Replication factor, number of partitions and topic name 
+# can be passed as the command line arguments
+# e.g.: ./create_topic.sh 3 1 topic-1
+# This would create the topic topic-1 with replication factor 3 and 1 partition
+
 #!/usr/bin/env bash
 
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor $1 --partitions $2 --topic $3
