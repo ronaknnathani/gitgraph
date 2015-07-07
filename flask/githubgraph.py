a80485bb7343248c72f8e8312be983aca4a1bd47
Ubuntu <ubuntu@ip-172-31-2-89.us-west-1.compute.internal>
2015-07-07 19:10:01 +0000

    api added

--- flask/githubgraph.py
+++ flask/githubgraph.py
@@ -7,6 +7,7 @@ import json
 from cassandra.query import SimpleStatement
 import operator
 from sets import Set
+from flask_restful import Resource, Api
 
 # importing script to fetch data from API in real time
 import fetch_data
@@ -16,6 +17,7 @@ cluster = Cluster(['52.8.127.252', '52.8.41.216'])
 session = cluster.connect('watch_events')
 
 app = Flask(__name__)
+api = Api(app)
 
 # homepage
 @app.route("/")
@@ -26,7 +28,7 @@ def hello():
 # trending repositories in the last week based on number of stars 
 @app.route("/weeklytrends")
 def weekly_trends():
-  cql = "SELECT * FROM top10weeklyrepos"; # querying cassandra to fetch the data
+  cql = "SELECT * FROM top10_weeklyrepos"; # querying cassandra to fetch the data
   stmt = session.execute(cql) # executing the query
   response = []
   for repo in stmt:
@@ -136,6 +138,87 @@ def get_graph(username):
   fetch_data.get_user_following(username) # calling the function in fetch_data.py to get data from API
   return 
 
+@app.route("/holla")
+def holla():
+  return "Holla World!"
+
+@app.route("/api/trending")
+def apitrending():
+  cql = "SELECT * FROM top10_weeklyrepos"; # querying cassandra to fetch the data
+  stmt = session.execute(cql) # executing the query
+  response = []
+  for repo in stmt:
+    response.append(repo) # getting the returned rows in a list
+  responselist = [(x.reponame,  x.watchcount) for x in response] # taking the reponames and corresponding watchcount
+  responselist.sort(key=operator.itemgetter(1), reverse = True) # sorting the repos based on watchcounts in descendng order
+  jsonresponse = [{"reponame":x[0], "watchcount":x[1]} for x in responselist] # creating a json response
+  return jsonify(Trending=jsonresponse) # returning the josnresponse to the template
+
+@app.route("/api/graph/<username>/<year>")
+def apigraph(username, year):
+  if year=="2015":
+    table = "userrepo"
+  elif year=="2014":
+    table = ["userrepo2014_1","userrepo2014_2"]
+  elif year=="2013":
+    table = "userrepo2013"
+  elif year=="2012":
+    table = "userrepo2012"
+  elif year=="Dec. 2011":
+    table = "userrepo2011"
+  elif year=="All":
+    table = ["userrepo2011", "userrepo2012", "userrepo2013", "userrepo2014_1","userrepo2014_2", "userrepo"]
+    
+  cql = "SELECT following from userfollow WHERE username=%s" # build the query
+  try:
+    stmt = session.execute(cql, parameters=[username]) # execute the query
+    if stmt==[]: # if the user details are not in the database, fetch from API
+      ans = get_graph(username) # get user details
+      if not ans:
+        return jsonify(Response="Records for "+username+" were not found")  # in case there is no result
+      stmt = session.execute(cql, parameters=[username]) # execute the query after getting details from API
+    
+    following_list = stmt[0].following # get the list of people followed by the user
+    if following_list==None: # if user doesn't follow anyone, return the corresponding response
+      return jsonify(Response=username+" is not following anyone")
+    
+    repojson = []
+    for all in following_list: # for all users that the given user follows get the respective repos
+      if year=="2014":
+        reporows = []
+        reporow = []
+        for i, x in enumerate(table):
+          reporows.append(session.execute("SELECT repo from "+ table[i] +" WHERE username=%s", parameters=[all]))
+        reporow = [x for y in reporows if y!=[] for x in Set(list(y[0].repo))]
+        if reporow==[]:
+          repojson.append({"username": all}) # if there are no repos the there are no children to the node, just people followed by the user
+        else:        
+          repojson.append({"username": all, "repositories": [reponame for reponame in reporow]}) # if there are repos that the user follows/contributes to, create the nested json for tree
+      
+      elif year=="All":
+        reporows = []
+        reporow = []
+        for i, x in enumerate(table):
+          reporows.append(session.execute("SELECT repo from "+ table[i] +" WHERE username=%s", parameters=[all]))
+        reporow = [x for y in reporows if y!=[] for x in Set(list(y[0].repo))]
+        if reporow==[]:
+          repojson.append({"username": all}) # if there are no repos the there are no children to the node, just people followed by the user
+        else:        
+          repojson.append({"username": all, "repositories": [reponame for reponame in reporow]}) # if there are repos that the user follows/contributes to, create the nested json for tree
+      
+      else:
+        cql = "SELECT repo from "+ table +" WHERE username=%s"
+        reporow = session.execute(cql, parameters=[all])
+        if reporow==[]:
+          repojson.append({"username": all}) # if there are no repos the there are no children to the node, just people followed by the user
+        else:        
+          repojson.append({"username": all, "repositories": [reponame for reponame in Set(list(reporow[0].repo))]}) # if there are repos that the user follows/contributes to, create the nested json for tree  
+    
+    jsonresponse = {"username": username, "following": [x for x in repojson]} # final json with parent and children for the tree
+    return jsonify(jsonresponse)
+  except:
+    return jsonify(Response="Records for "+username+" were not found")
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0')
 
